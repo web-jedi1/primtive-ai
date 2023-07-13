e692411de4512e8ed722e5054e06a82d71b95568
web-jedi1 <web_jedi1@protonmail.ch>
2023-07-13 22:56:12 +0200

    someoptimizations, trying new activation func.

--- dual-input/build.sh
+++ dual-input/build.sh
@@ -3,4 +3,5 @@ DIR="/home/manfred/dev/primtive-ai/dual-input"
 cd $DIR
 #ITER=500
 gcc ./dual-input.c -o ./main -lm
-./main 500 && rm main
+./main 50000
+rm main
--- dual-input/dual-input.c
+++ dual-input/dual-input.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
 
-/*
+
 // and gate
 int data[4][3] = {
     {0, 0, 0},
@@ -11,9 +10,9 @@ int data[4][3] = {
     {1, 0, 0},
     {1, 1, 1}
     // in, in, out
-}
-*/
+};
 
+/*
 // or gate
 int data[4][3] = {
     {0, 0, 0},
@@ -22,7 +21,7 @@ int data[4][3] = {
     {1, 1, 1}
     // in, in, out
 };
-
+*/
 #define size sizeof(data)/sizeof(data[0])
 #define rand_float (float) rand() / (float) RAND_MAX 
 
@@ -30,29 +29,52 @@ float sigmoid (float x) {
     return 1.f / (1.f + expf(x));
 }
 
+/*
+// can use this activation function to better model & gate?
+float heaviside_step (float x) {
+    return (x >= 0.5f) ? 1.f: 0.f;
+}
+*/
+
 // y = w1*x1 + w1*x2 + b
-float cost(float w1, float w2) {
-    float result = 0.0f;
+float cost (float w1, float w2) {
+    float mse = 0.0f;
     for (size_t i = 0; i < size; i++) {
         float x1 = data[i][0];
         float x2 = data[i][1];
         float y = sigmoid(x1*w1 + x2*w2);
-        // compute MSE
-        float mse = pow((y - data[i][2]), 2)
+        mse += pow((y - data[i][2]), 2);
     }
-    result /= size;
-    return result;
+    mse /= size;
+    return mse;
 }
 
-
+void printModel (float w1, float w2) {
+    printf("\n--------------------------------------\n\n");
+    for (size_t i = 0; i < size; i++) {
+        printf("%d | %d = %f (expected: %d)\n", data[i][0], data[i][1], sigmoid(data[i][0]*w1 + data[i][1]*w2), data[i][2]);
+    }
+}
 
 int main (int argc, char *argv) {
 
-    srand(420);
-    float w1 = rand_float()*10.0f;
-    float w2 = rand_float()*10.0f;
+    if (argc > 2) {fprintf(stderr, "Usage: ./main [(int) iterations]\n"); return 1;}
+    int iterations = 50000;//atoi(&argv[1]);
 
-    // compute model
+    srand(69);
+    float mu = 1e-1;
+    float rate = 1e-1;
+    float w1 = rand_float*10.0f;
+    float w2 = rand_float*10.0f;
 
+    for (size_t i = 0; i < iterations; i++) {
+        float c = cost(w1, w2);
+        printf("c: %f, w1: %f, w2: %f\n", c, w1, w2);
+        float dw1 = (cost(w1 + mu, w2) - c)/mu;
+        float dw2 = (cost(w1, w2 + mu) - c)/mu;
+        w1 -= rate*dw1;
+        w2 -= rate*dw2;
+    }
+    printModel(w1, w2);
     return 0;
 }
