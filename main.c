488fd11780565abc44338c3473015663dbad8a9c
web-jedi1 <web_jedi1@protonmail.ch>
2023-07-13 13:40:48 +0200

    added bias to model

--- main.c
+++ main.c
@@ -17,17 +17,16 @@ float rand_float(void) {
     return (float) rand() / (float) RAND_MAX;
 }
 
-float cost (float w) { // cost function accepts entire model as input, we want to minmize the cost function
+float cost (float w, float b) { // cost function accepts entire model as input, we want to minmize the cost function
     float err = 0.0f;
     for (size_t i = 0; i < dat_size; i++) {
         float x = dat[i][0]; // out input x
-        float y = x*w;  // feed data into model and return output y
+        float y = x*w + b;  // feed data into model and return output y
         //printf("actual: %f, expected %f\n", y, (float) dat[i][1]); // see results
 
         // compute MSE: MSE = (y-dat[i][1])^2
         err += pow((y - dat[i][1]), 2);
     }
-
     err /= dat_size; // divide sum by total to get mean square error, measure of how badly model performs. This is what we minimize.
     return err;
 }
@@ -57,28 +56,28 @@ int main (int argc, char *argv[]) {
     // y = x*w;
     float mu = 1e-3, rate = 1e-3;
     float w = rand_float()*10.0f; // the models single parameter
+    float b = rand_float()*5.0f; // bias
 
     for (size_t i = 0; i < iterations; i++) {
 
         // COMPUTE OPTIMUM OF COST FUNCTION
         // change to actually compute true derivative instead of finite diff
         // what happens of cost function has multiple optimal solutions?
-
-        float dcost = (cost(w + mu) - cost(w))/mu;  // compute finite difference, change this to take actual derivative of cost func.
-        //printf("dcost: %f\n", dcost);
+        float c = cost(w, b);
+        float dcost = (cost(w + mu, b) - c)/mu;  // compute finite difference, change this to take actual derivative of cost func.
+        float dbias = (cost(w, b + mu) - c)/mu;
+        printf("cost: %f, w: %f, b: %f\n", c, w, b);
         //printf("[-] MSE pre opt: %f\n", cost(w)); // cost function before finite difference update (lim->0)
         w -= rate*dcost;
+        b -= rate*dbias;
         //printf("[+] MSE: %f\n", cost(w)); // cost function after finite difference update
     }
 
     printf("---------------------------\n");
-    printf("parameter value: %f\n", w);
+    printf("dcost: %f, dbias: %f\n", w, b);
 
     printf("---------------------------\n");
     print_model(w);
-
-
-
 
     return 0;
 
